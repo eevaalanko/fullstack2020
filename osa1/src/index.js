2915545e413e80da4b5227139c862c77c76880a0
Eeva Alanko <eeva.alanko@siili.com>
2020-07-23 15:30:32 +0300

    Add exercise 1.4

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -3,20 +3,15 @@ import ReactDOM from "react-dom";
 
 const Header = ({ course }) => <h1>{course}</h1>;
 
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ part1, part2, part3 }) => (
+const Content = ({ content }) => (
   <div>
-    <Part part={part1} />
-    <Part part={part2} />
-    <Part part={part3} />
+    {content.map((content) => (
+      <p>
+        {content.name} {content.exercises}
+      </p>
+    ))}
   </div>
 );
-
 const Total = ({ text, content }) => (
   <p>
     {text} {content.reduce((acc, curr) => acc + curr.exercises, 0)}
@@ -27,37 +22,24 @@ const App = () => {
   const course = "Half Stack application development";
   const content = [
     {
-      text: "Fundamentals of React",
+      name: "Fundamentals of React",
       exercises: 10,
     },
     {
-      text: "Using props to pass data",
+      name: "Using props to pass data",
       exercises: 7,
     },
     {
-      text: "State of a component",
+      name: "State of a component",
       exercises: 14,
     },
   ];
   const totalText = "Number of exercises";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
-  const props = { part1, part2, part3 };
 
   return (
     <div>
       <Header course={course} />
-      <Content {...props} />
+      <Content content={content} />
       <Total text={totalText} content={content} />
     </div>
   );
