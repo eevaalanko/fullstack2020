5ed5560a373d4d09c998b2f9f103a04f72aa91c2
Eeva Alanko <eeva.alanko@siili.com>
2020-07-26 10:12:09 +0300

    Add exercise 1.9

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -12,12 +12,18 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      {all > 0 ? (
+        <div>
+          <p>good {good}</p>
+          <p>neutral {neutral}</p>
+          <p>bad {bad}</p>
+          <p>all {all}</p>
+          <p>average {average}</p>
+          <p>positive {positive} %</p>
+        </div>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   );
 };
