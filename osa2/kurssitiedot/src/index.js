74e9f197ccdc752565b727ffa0f98debf3d5866a
Eeva Alanko <eeva.alanko@siili.com>
2020-07-26 16:18:27 +0300

    Add exercise 2.5

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,37 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const Header = ({ course }) => <h1>{course}</h1>;
-
-const Part = ({ name, exercises }) => (
-  <p>
-    {name} {exercises}
-  </p>
-);
-
-const Content = ({ content }) => (
-  <div>
-    {content.map((content) => (
-      <Part {...content} key={content.id} />
-    ))}
-  </div>
-);
-const Total = ({ content }) => (
-  <p>
-    <b>
-      total of {content.reduce((acc, curr) => acc + curr.exercises, 0)}{" "}
-      exercises
-    </b>
-  </p>
-);
-
-const Course = ({ name, parts }) => (
-  <div>
-    <Header course={name} />
-    <Content content={parts} />
-    <Total content={parts} />
-  </div>
-);
+import Course from "./Course";
 
 const App = () => {
   const courses = [
