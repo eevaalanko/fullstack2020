51492345b8ca17732f2ebc43d58db9b88c11d236
Eeva Alanko <eeva.alanko@siili.com>
2020-07-26 10:08:59 +0300

    Add exercise 1.9

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,38 +1,40 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-const Button = ({handleClick, text}) => (
-    <button onClick={handleClick}>
-        {text}
-    </button>
-)
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = all > 0 ? (good - bad) / all : 0;
+  const positive = all > 0 ? (good / all) * 100 : 0;
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
+  );
+};
 
 const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Statistics good={good} bad={bad} neutral={neutral} />
+    </div>
+  );
+};
 
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
-    const all = good + neutral + bad;
-    const average = all > 0 ? (good - bad) / all : 0
-    const positive = all > 0 ? (good / all) * 100 : 0
-
-    return (
-        <div>
-            <h1>Give feedback</h1>
-            <Button handleClick={() => setGood(good + 1)} text="good" />
-            <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-            <Button handleClick={() => setBad(bad + 1)} text="bad" />
-            <h1>Statistics</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {all}</p>
-            <p>average {average}</p>
-            <p>positive {positive} %</p>
-        </div>
-    )
-}
-
-ReactDOM.render(<App />,
-    document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById("root"));
