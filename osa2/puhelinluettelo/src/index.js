a25b118c67b297cc0a213854a2ce26c42ab5afde
Eeva Alanko <eeva.alanko@siili.com>
2020-07-31 14:55:05 +0300

    Add exercise 2.11

--- osa2/puhelinluettelo/src/index.js
+++ osa2/puhelinluettelo/src/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
 const Filter = ({ handleChange, newFilter }) => (
   <div>
@@ -61,12 +62,16 @@ const Persons = ({ persons }) => (
 );
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123123" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setNewFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleNameChange = (event) => setNewName(event.target.value);
   const handleNumberChange = (event) => setNewNumber(event.target.value);
