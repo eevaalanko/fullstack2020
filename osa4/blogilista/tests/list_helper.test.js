ad90ccc45943cc6ea8bbb004987bdd312a358bcd
Eeva Alanko <eeva.alanko@siili.com>
2020-08-15 10:39:56 +0300

    Added exercise 4.4

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -6,3 +6,19 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    expect(listHelper.totalLikes([])).toBe(0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const testBlogs = [{ id: '1', likes: 5 }]
+    expect(listHelper.totalLikes(testBlogs)).toEqual(testBlogs[0].likes)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const testBlogs = [{ id: '1', likes: 22 }, { id: '2', likes: 3 }, { id: '3', likes: 6 }]
+    expect(listHelper.totalLikes(testBlogs)).toEqual(31)
+  })
+})
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,9 +1,16 @@
-
 const dummy = (blogs) => {
   console.log('blogs: ', blogs)
   return 1
 }
 
+const totalLikes = (blogs) => {
+  console.log('blogs: ', blogs)
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((amount, blog) => amount + blog.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 }
