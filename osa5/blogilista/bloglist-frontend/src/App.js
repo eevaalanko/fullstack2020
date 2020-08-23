ef8187406ff0cfbe24eef4c789f1cc6fe12dc957
Eeva Alanko <eeva.alanko@siili.com>
2020-08-23 13:02:43 +0300

    Added exercise 5.3

--- osa5/blogilista/bloglist-frontend/src/App.js
+++ osa5/blogilista/bloglist-frontend/src/App.js
@@ -6,6 +6,10 @@ import Notification from "./components/Notification";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [newTitle, setNewTitle] = useState("");
+  const [newAuthor, setNewAuthor] = useState("");
+  const [newLink, setNewLink] = useState("");
+  const [newLikes, setNewLikes] = useState(0);
   const [errorMessage, setErrorMessage] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -23,6 +27,37 @@ const App = () => {
       blogService.setToken(user.token);
     }
   }, []);
+
+  const addBlog = (event) => {
+    event.preventDefault();
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newLink,
+      likes: newLikes,
+    };
+
+    blogService.create(blogObject).then((returnedBlog) => {
+      setBlogs(blogs.concat(returnedBlog));
+      setNewTitle("");
+      setNewAuthor("");
+      setNewLink("");
+      setNewLikes(0);
+    });
+  };
+
+  const handleTitleChange = (event) => {
+    setNewTitle(event.target.value);
+  };
+  const handleAuthorChange = (event) => {
+    setNewAuthor(event.target.value);
+  };
+  const handleLinkChange = (event) => {
+    setNewLink(event.target.value);
+  };
+  const handleLikesChange = (event) => {
+    setNewLikes(event.target.value);
+  };
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -86,6 +121,24 @@ const App = () => {
       <p>
         {user.name} logged in <button onClick={logout}>logout</button>
       </p>
+      <h2>create new</h2>
+      <form onSubmit={addBlog}>
+        <p>
+          Title: <input value={newTitle} onChange={handleTitleChange} />
+        </p>
+        <p>
+          Author: <input value={newAuthor} onChange={handleAuthorChange} />
+        </p>
+        <p>
+          Link: <input value={newLink} onChange={handleLinkChange} />
+        </p>
+        <p>
+          Likes: <input type="number" value={newLikes} onChange={handleLikesChange} />
+        </p>
+
+        <button type="submit">save</button>
+      </form>
+      <br/>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
