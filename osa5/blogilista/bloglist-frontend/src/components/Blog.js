841308e62dd8d859cab20c77e009a2b3933c967d
Eeva Alanko <eeva.alanko@siili.com>
2020-08-26 16:24:02 +0300

    Added exercises 5.13, 5.14 and 5.15

--- osa5/blogilista/bloglist-frontend/src/components/Blog.js
+++ osa5/blogilista/bloglist-frontend/src/components/Blog.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import Togglable from './Togglable'
 
 const blogStyle = {
   paddingTop: 10,
@@ -9,17 +8,37 @@ const blogStyle = {
   borderWidth: 1,
   marginBottom: 5,
 }
-const Blog = ({ blog, addLike, removeBlog }) => (
-  <div style={blogStyle}>
-    {blog.title} {blog.author}
-    <Togglable buttonLabel="view" closeButtonLabel="hide">
-      <p>link: {blog.url}</p>
-      <p>likes: {blog.likes}<button onClick={()=> addLike(blog.id)}>like</button></p>
-      <p>user: {blog.user.name}</p>
-      <button onClick={()=> removeBlog(blog.id)}>remove</button>
-    </Togglable>
-  </div>
-)
+
+const Blog = ({ blog, addLike, removeBlog }) => {
+  const [visible, setVisible] = useState(false)
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      {`${blog.title} ${blog.author} `}
+      <button style={hideWhenVisible} onClick={toggleVisibility}>
+        show
+      </button>
+      <button style={showWhenVisible} onClick={toggleVisibility}>
+        hide
+      </button>
+      <div style={showWhenVisible} id="blog-info">
+        <p id="link">link: {blog.url}</p>
+        <p>
+          {`likes: ${blog.likes} `}
+          <button onClick={() => addLike(blog.id)}>like</button>
+        </p>
+        <p>user: {blog.user.name}</p>
+        <button onClick={() => removeBlog(blog.id)}>remove</button>
+      </div>
+    </div>
+  )
+}
 
 Blog.propTypes = {
   blog: PropTypes.shape({
@@ -28,7 +47,7 @@ Blog.propTypes = {
     author: PropTypes.string,
     url: PropTypes.string,
     likes: PropTypes.number,
-    user: PropTypes.shape({name: PropTypes.string})
+    user: PropTypes.shape({ name: PropTypes.string }),
   }).isRequired,
   addLike: PropTypes.func.isRequired,
   removeBlog: PropTypes.func.isRequired,
