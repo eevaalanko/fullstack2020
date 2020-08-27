d367e44c6b2fa8a2d1f10925c031b0dd7c77f14f
Eeva Alanko <eeva.alanko@siili.com>
2020-08-27 22:42:41 +0300

    Added exercise 6.17

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,6 +1,5 @@
 import anecdoteService from "../services/anecdotes";
 
-
 export const createAnecdote1 = (data) => {
   return {
     type: "NEW_ANECDOTE",
@@ -8,33 +7,36 @@ export const createAnecdote1 = (data) => {
   };
 };
 
-export const createAnecdote = content => {
-  return async dispatch => {
-    const newAnecdote = await anecdoteService.createNew(content)
+export const createAnecdote = (content) => {
+  return async (dispatch) => {
+    const newAnecdote = await anecdoteService.createNew(content);
     dispatch({
       type: "NEW_ANECDOTE",
       data: newAnecdote,
-    })
-  }
-}
-
-export const voteAnecdote = (anecdote) => {
-  return {
-    type: "ADD_VOTE",
-    anecdote,
+    });
   };
 };
 
-
-export const initializeAnecdotes  = () => {
-  return async dispatch => {
-    const anecdotes = await anecdoteService.getAll()
+export const voteAnecdote = (content) => {
+  console.log("content ooon   ", content);
+  return async (dispatch) => {
+    const newAnecdote = await anecdoteService.update(content.id, content);
     dispatch({
-      type: 'INIT_ANECDOTES',
+      type: "ADD_VOTE",
+      data: newAnecdote,
+    });
+  };
+};
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch({
+      type: "INIT_ANECDOTES",
       data: anecdotes,
-    })
-  }
-}
+    });
+  };
+};
 
 const initialState = [];
 
@@ -46,8 +48,8 @@ const anecdoteReducer = (state = initialState, action) => {
       return action.data;
     case "ADD_VOTE": {
       return state.map((el) =>
-        el.id === action.anecdote.id
-          ? { ...action.anecdote, votes: action.anecdote.votes + 1 }
+        el.id === action.data.id
+          ? { ...action.data, votes: action.data.votes + 1 }
           : el
       );
     }
