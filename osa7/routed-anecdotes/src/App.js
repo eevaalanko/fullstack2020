d5603d0a84cc8a809c8bb6c108399664babb7799
Eeva Alanko <eeva.alanko@siili.com>
2020-09-06 17:55:38 +0300

    Added exercise 7.4

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
+import { useField } from "./hooks";
 import {
   Switch,
   Route,
   Link,
-  Redirect,
   useRouteMatch,
   useHistory,
 } from "react-router-dom";
@@ -87,17 +87,18 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
+  const content = useField("text");
+  const author = useField("text");
+  const info = useField("text");
+
   const history = useHistory();
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
     history.push("/");
@@ -109,27 +110,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
@@ -164,8 +153,8 @@ const App = () => {
       "a new anecdote " + anecdote.content + " has been created!"
     );
     setTimeout(() => {
-      setNotification(null)
-    }, 10000)
+      setNotification(null);
+    }, 10000);
   };
 
   const anecdoteById = (id) => anecdotes.find((a) => a.id === id);
