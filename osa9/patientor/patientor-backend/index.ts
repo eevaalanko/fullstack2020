28e0aea1e6a858825fb0b78652c24866f8b49a64
Eeva Alanko <eeva.alanko@siili.com>
2020-11-02 18:45:16 +0200

    Add exercises 9.8 and 9.9

---
+++ osa9/patientor/patientor-backend/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/ping", (_req, res) => {
+  res.send("pong");
+});
+
+app.get("/hello", (_req, res) => {
+  res.send("Hello Full Stack!");
+});
+
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
