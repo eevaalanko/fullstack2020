316ada9cb04c6123730601fc225593f42b19009e
Eeva Alanko <eeva.alanko@siili.com>
2020-11-03 16:11:59 +0200

    Add exercises 9.18. fix git commit problems

---
+++ osa9/patientor/patientorfrontend/src/components/PatientComponent.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import axios from "axios";
+import { Icon, Item, Container, Button } from "semantic-ui-react";
+import { Patient } from "../types";
+import { apiBaseUrl } from "../constants";
+import { useStateValue } from "../state";
+import { useParams } from "react-router-dom";
+import { updatePatient } from "../state";
+
+const PatientComponent: React.FC = () => {
+  const [{ patients }, dispatch] = useStateValue();
+  console.log("state? :", [{ patients }, dispatch]);
+  console.log("patients:", patients);
+
+  const { id } = useParams<{ id: string }>();
+  console.log("id:", id);
+  const patient = patients[id];
+  console.log("patient:", patient);
+
+  const genderIcons = {
+    male: { name: "mars" as "mars" },
+    female: { name: "venus" as "venus" },
+    other: { name: "other gender vertical" as "other gender vertical" },
+  };
+
+  React.useEffect(() => {
+    const fetchPatient = async () => {
+      try {
+        const { data: patientFromApi } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+        dispatch(updatePatient(patientFromApi));
+        console.log("patientFromApi:", patientFromApi);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    fetchPatient();
+  }, [id, dispatch]);
+
+  if (!patient) return null;
+  if (!patient.entries) return null;
+
+  return (
+    <Container>
+      <h1>
+        {patient.name} <Icon {...genderIcons[patient.gender]} />
+      </h1>
+
+      <div>
+        <b>snn: {patient.ssn}</b>
+      </div>
+      <div>
+        <b>occupation: {patient.occupation}</b>
+      </div>
+    </Container>
+  );
+};
+
+export default PatientComponent;
---
+++ osa9/patientor/patientorfrontend/src/state/reducer.ts
@@ -0,0 +1,82 @@
+import { State } from "./state";
+import { Patient, Diagnosis, Entry } from "../types";
+
+export type Action =
+  | {
+      type: "SET_PATIENT_LIST";
+      payload: Patient[];
+    }
+  | {
+      type: "ADD_PATIENT";
+      payload: Patient;
+    }
+  | {
+      type: "UPDATE_PATIENT";
+      payload: Patient;
+    }
+  | {
+      type: "SET_DIAGNOSIS_LIST";
+      payload: Diagnosis[];
+    }
+  | {
+      type: "ADD_ENTRY";
+      payload: Entry;
+    };
+
+export const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "SET_PATIENT_LIST":
+      return {
+        ...state,
+        patients: {
+          ...action.payload.reduce(
+            (memo, patient) => ({ ...memo, [patient.id]: patient }),
+            {}
+          ),
+          ...state.patients,
+        },
+      };
+    case "ADD_PATIENT":
+      return {
+        ...state,
+        patients: {
+          ...state.patients,
+          [action.payload.id]: action.payload,
+        },
+      };
+    case "UPDATE_PATIENT":
+      return {
+        ...state,
+        patients: {
+          ...state.patients,
+          [action.payload.id]: {
+            ...state.patients[action.payload.id],
+            ...action.payload,
+          },
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+export const setPatientList = (patients: Patient[]): Action => {
+  return {
+    type: "SET_PATIENT_LIST",
+    payload: patients,
+  };
+};
+
+export const setDiagnosisList = (diagnoses: Diagnosis[]): Action => {
+  return {
+    type: "SET_DIAGNOSIS_LIST",
+    payload: diagnoses,
+  };
+};
+
+export const updatePatient = (patient: Patient): Action => {
+  return {
+    type: "UPDATE_PATIENT",
+    payload: patient,
+  };
+};
